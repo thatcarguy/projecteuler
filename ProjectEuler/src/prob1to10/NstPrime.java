8af88c39bde1eaa5859accfe47fcacc8ebb55d0e
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2020-02-02 21:24:00 -0700

    Problems 12

--- ProjectEuler/src/prob1to10/NstPrime.java
+++ ProjectEuler/src/prob1to10/NstPrime.java
@@ -1,5 +1,13 @@
 package prob1to10;
 
+/**
+ * Problem 7
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+ * @author Duke
+ *
+ */
 public class NstPrime {
 	public static void main(String[] args){
 		System.out.println(findPrime(6));
